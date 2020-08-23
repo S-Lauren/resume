2f87dd1b2ea7571b74879653da5567d84c7a6435
S-Lauren <s.lauren.contact@gmail.com>
2020-08-23 18:08:49 +0200

    adding sass

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/app.scss'
+import Header from './components/Header/header'
+
+
+function App() {
+  return (
+    <>
+<div  className="test">
+<Header />
+</div>
+</>
+    
+
+  );
+}
+
+export default App;
---
+++ src/components/Header/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header.css'
+
+const  Header = () => {
+  return (
+    <div className="header">
+		<div className="container">
+			<div className="row-center">
+				<div className="logo">
+          <a href="#">Sarah Lauren G.</a>
+				</div>
+				<nav>
+					<span></span>
+					<span></span>
+					<span></span>
+				</nav>
+			</div>
+        </div>
+    </div>
+  )
+}
+export default Header;
